d26824ee891d3d8e4536a927ceee652782411ca8
Pedro Mass <drom296@gmail.com>
2012-02-13 18:08:54 -0500

    Clearing cookies

--- logout.php
+++ logout.php
@@ -1,9 +1,19 @@
 <?php
 session_start();
+
+//include any required libraries/classes
+function __autoload($className) {
+	require_once 'classes/' . $className . '.class.php';
+}
+
 unset($_SESSION);
 session_destroy();
 
 // destroy the cookie
+Utils::expireCookie("username");
+
+// unset the cookie
+unset($_COOKIE['username']);
 
 header("Location: login.php");
 ?>
