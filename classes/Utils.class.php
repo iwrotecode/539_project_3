3ba0510722fd11a2bb4aac426bc05340d55947a1
Pedro Mass <drom296@gmail.com>
2012-02-13 17:47:25 -0500

    Added a setcookie class

--- classes/Utils.class.php
+++ classes/Utils.class.php
@@ -24,13 +24,27 @@
 				return self::$sessionVar;
 			}
 			
+			/**
+			 * Sets a cookie based on the passed in values.
+			 * 
+			 * Defaults are sepecified in the class as properties
+			 */
 			static function setcookie($name, $value, $expire, $path=null, $domain=null, $secure=null){
-				if(!$path){
-					
+				
+				// check if we have variables passed in, if not, use the defaults defined 
+				// in the class as properties
+				if(is_null($path)){
+					$path = self::$path;
+				}
+				if(is_null($domain)){
+					$domain = self::$domain;
+				}
+				if(is_null($secure)){
+					$secure = self::$secure;
 				}
 				
-				
-				setcookie($name, $value, $expire, $path, $domain, $secure);
+				// set the cookie
+				return setcookie($name, $value, $expire, $path, $domain, $secure);
 			}
 		}
 ?>
