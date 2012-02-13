2c2fdb0f5e2c49e9863d8706b14f024595ee8a70
Pedro Mass <drom296@gmail.com>
2012-02-13 18:08:37 -0500

    Added an expire cookie function

--- classes/utils.class.php
+++ classes/utils.class.php
@@ -6,6 +6,7 @@
 			private static $path = "/~pjm8632/";
 			private static $domain = "nova.it.rit.edu";
 			private static $secure = false;
+			private static $daysExpire = 3;
 			
 			static function getSessionVarValue(){
 				// get ip address
@@ -29,7 +30,7 @@
 			 * 
 			 * Defaults are sepecified in the class as properties
 			 */
-			static function setcookie($name, $value, $expire, $path=null, $domain=null, $secure=null){
+			static function setCookie($name, $value, $expire=null, $path=null, $domain=null, $secure=null){
 				
 				// check if we have variables passed in, if not, use the defaults defined 
 				// in the class as properties
@@ -42,9 +43,30 @@
 				if(is_null($secure)){
 					$secure = self::$secure;
 				}
+				if(is_null($expire) || !is_int($expire)){
+					// seconds in a day: 86400
+					$expire = (time() + 86400) *self::$daysExpire;
+				}
 				
 				// set the cookie
 				return setcookie($name, $value, $expire, $path, $domain, $secure);
+			}
+			
+			/**
+			 * Clears the specified cookie, and changes its expiration to the number of
+			 * seconds specified.
+			 * 
+			 * @param name - name of the cookie
+			 * @param $expire [optional]- seconds to expire, default is 3 days ago 
+			 */
+			static function expireCookie($name, $expire=null){
+				// if they specified a number for expire, then negate it if not already	
+				if(is_int($expire) && $expire > 0){
+					$expire = -$expire;
+				}
+				
+				// expire the cookie and return
+				return self::setCookie($name, "", $expire);
 			}
 		}
 ?>
