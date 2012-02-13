c28e3ba6bd1498c8caf00ac777b7edc76cdfb20a
Pedro Juan <drom296@gmail.com>
2012-02-13 16:40:21 -0500

    Fixed to make use of our page class functions

--- admin.php
+++ admin.php
@@ -1,16 +1,26 @@
 <?php
 //start the session
+session_start();
+ob_start();
 
 //if not logged in, re-direct to login.php
 
 //include any libraries/classes needed
+function __autoload($className) {
+	require_once 'classes/' . $className . '.class.php';
+}
 
+
+// start the page
 echo Page::header("Admin Page");
 //for the actual project you might want to check access level at this point
 
-echo Page::navigation();
+// add navigation
+echo Page::addNav();
 
 //output the session variables and cookies
 
 echo Page::footer();
+
+ob_end_flush();
 ?>
