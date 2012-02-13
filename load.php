16dd53ac764b591e3815a069521d8c499b411a0f
Pedro Mass <drom296@gmail.com>
2012-02-12 21:53:30 -0500

    Tested the select function with the load menu

--- load.php
+++ load.php
@@ -1,5 +1,6 @@
 <?php
 	require_once("classes/page.class.php");
+	require_once("classes/form.class.php");
 
 	$styles = "css/nav.css";
 
@@ -10,6 +11,10 @@
 	echo Page::addNav();
 	
 	// add content
+	
+	// add table select
+	$tables = array("cms_banner", "cms_news", "cms_editorial");
+	echo Form::buildSelect($tables, "table");
 	
 	// end the page
 	echo Page::footer();
