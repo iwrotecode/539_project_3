2b0a0a24930678d7d4e0489ff79f6916f75e89a4
Pedro Juan <drom296@gmail.com>
2012-02-13 16:48:16 -0500

    Displayed the Session variables and cookie values if they are logged in

--- admin.php
+++ admin.php
@@ -4,12 +4,20 @@ session_start();
 ob_start();
 
 //if not logged in, re-direct to login.php
+// get session var and value
+$sessionVar = Utils::getSessionVar();
+$sessionValue = Utils::getSessionVarValue();
+
+// check if it is set and equals the value
+if (!isset($_SESSION[$sessionVar]) || ($_SESSION[$sessionVar] != $sessionVar)) {
+	// they do, so redirect to admin.php
+	header("Location: login.php");
+}
 
 //include any libraries/classes needed
 function __autoload($className) {
 	require_once 'classes/' . $className . '.class.php';
 }
-
 
 // start the page
 echo Page::header("Admin Page");
@@ -19,6 +27,15 @@ echo Page::header("Admin Page");
 echo Page::addNav();
 
 //output the session variables and cookies
+echo "<p>Session Vars</p>";
+foreach ($_SESSION as $k => $v) {
+	echo "$k=$v<br />";
+}
+
+echo "<p>Cookies</p>";
+foreach ($_COOKIE as $k => $v) {
+	echo "$k=$v<br />";
+}
 
 echo Page::footer();
 
