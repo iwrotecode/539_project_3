c1fb23d6a6c33a25b6597059fa272aa80ab679a6
Pedro Mass <drom296@gmail.com>
2012-02-12 22:30:47 -0500

    Added an input for the delimiter

--- load.php
+++ load.php
@@ -1,22 +1,47 @@
 <?php
-	require_once("classes/page.class.php");
-	require_once("classes/form.class.php");
+require_once ("classes/page.class.php");
+require_once ("classes/form.class.php");
 
-	$styles = "css/nav.css";
+$passed = false;
 
-	// start the page
-	echo Page::header("load.php", $styles);
-	
-	// add navigation
-	echo Page::addNav();
-	
-	// add content
-	
+// start the page
+echo Page::header("load.php");
+
+// add navigation
+echo Page::addNav();
+
+// add content
+// display a title for the page
+echo "<h1>Import Data</h1>";
+
+// check if they passed
+if (!$passed) {
+
+	// add the form
+	echo "<form>\n";
+
 	// add table select
 	$tables = array("cms_banner", "cms_news", "cms_editorial");
+	echo "<label for=\"table\">Table Name: </label>";
 	echo Form::buildSelect($tables, "table");
 	
-	// end the page
-	echo Page::footer();
+	// add the file name
+	echo "<label for=\"filename\">File Name: </label>";
+	echo "<input name=\"filename\" size=\"30\"></input>";
+	
+	// add the delimiter name
+	echo "<label for=\"filename\">Delimiter: </label>";
+	echo "<input name=\"delimiter\" size=\"5\"></input>";
+
+	// add the get info submit button
+	echo "<input type=\"submit\" value=\"Get Table Info\"/>";
+
+	echo "</form>\n";
+} else {
+	// since they passed, build the other form
+}
+
+// end the page
+echo Page::footer();
 ?>
 
