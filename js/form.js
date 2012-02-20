86f8a118f748aad94ca96ae8683a711d0163ff7f
Pedro Juan <drom296@gmail.com>
2012-02-20 15:56:32 -0500

    Working on the lightbox feature for big inputs

--- js/form.js
+++ js/form.js
@@ -8,4 +8,33 @@ $(document).ready(function(){
 		// change the type to email
 		this.setAttribute("type","email");
 	});
+	
+	// check if the value for input exceeds what the input holds
+	$('form input').focus(function(){
+		// get the length of the input
+		var inputWidth = $(this).width();
+		
+		// temporarily create a span to mesure the width
+		var temp = document.createElement("span");
+		// append the value to the width
+		temp.appendChild(document.createTextNode($(this).val())); 
+		
+		// append span to the body
+		document.body.appendChild(temp);
+		
+		// grab the width
+		var valueWidth = $(temp).width()-27;
+		
+		// remove the span
+		document.body.removeChild(temp);
+	
+		// check if the width is bigger than the input
+		if(inputWidth < valueWidth){
+			// do the lightbox 
+		}
+		
+		console.log("iWidth: "+inputWidth+" | uWidth: "+valueWidth);
+		
+	});
+	
 });
