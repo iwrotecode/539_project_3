dc88fbe3f38be2fd560ca16bc334ef571f916807
Pedro Juan <drom296@gmail.com>
2012-02-20 20:57:46 -0500

    tried to get lightbox working

--- js/form.js
+++ js/form.js
@@ -9,12 +9,31 @@ $(document).ready(function(){
 		this.setAttribute("type","email");
 	});
 	
+	// build the div for the lightbox
+	if($('form input')){
+		// add the div for the lightbox
+		var lightBox = document.createElement("div");
+		lightBox.setAttribute("style","display:none");
+		lightBox.setAttribute("id","lightboxDiv");
+		
+		var data = document.createElement("div");
+		data.setAttribute("id","data");
+		
+		var text = document.createElement("textarea");
+		
+		data.appendChild(text);
+		
+		lightBox.appendChild(data);
+		
+		document.body.appendChild(lightBox);
+	}
+	
 	// check if the value for input exceeds what the input holds
 	$('form input').focus(function(){
 		// get the length of the input
 		var inputWidth = $(this).width();
 		
-		var value = $(this).val();s
+		var value = $(this).val();
 		
 		// temporarily create a span to mesure the width
 		var temp = document.createElement("span");
@@ -33,8 +52,7 @@ $(document).ready(function(){
 		// check if the width is bigger than the input
 		if(inputWidth < valueWidth){
 			// do the lightbox 
-			
-			
+			$("#lightboxDiv").fancybox();	
 		}
 		
 		console.log("iWidth: "+inputWidth+" | uWidth: "+valueWidth);
